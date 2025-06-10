bceb68b4bff514ab5ca127abc70b155b5865cd62
Thiago Chirana <engsw.thiago@gmail.com>
2025-06-10 01:41:15 -0300

    fix: adjust initializer

--- lib/generators/kaleo/templates/initializer.rb
+++ lib/generators/kaleo/templates/initializer.rb
@@ -1,9 +1,9 @@
 Rails.application.config.after_initialize do
   Kaleo.configure do |config|
     config.user_class = User.name # or the KlassUser.name
-    config.trial_days = 14
+    config.trial_days = 7.days.from_now
     config.mailer_sender = 'no-reply@minhaapp.com'
 
-    config.invitation_valid_for = 
+    config.invitation_valid_for = 2.hours.from_now
   end
 end
